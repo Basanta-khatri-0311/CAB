0914d59f207a96cb239b86ad627bcbae5e0d9ed2
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2026-04-22 00:20:21 +0545

    feat: add SPA redirect rules, define start script, and remove static upload serving

--- server/src/app.js
+++ server/src/app.js
@@ -7,9 +7,6 @@ const publicRoutes = require('./routes/public.routes');
 const postRoutes = require('./routes/post.routes');
 const userRoutes = require('./routes/user.routes');
 const uploadRoutes = require('./routes/upload.routes');
-const path = require('path');
-const fs = require('fs');
-
 const helmet = require('helmet');
 const compression = require('compression');
 
@@ -37,7 +34,6 @@ app.use(cors({
   credentials: true,
 }))
 app.use(express.json()) //for allowing the json type
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use('/api/projects', projectRoutes)
 app.use('/api/finances', financeRoutes)
 app.use("/api/auth", authRoutes);
